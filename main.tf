affaaf83c0d13b1ab9a362d6b3e2ebf4e99490ed
sandeep-nadig <sandeepnadig@gmail.com>
2026-05-03 22:27:40 +0530

    create SA account in gcp

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-test"
+resource "google_service_account" "test_sa" {
+  account_id   = "test-sa-terraform"
+  display_name = "Terraform Service Account"
+}
+
+resource "google_project_iam_member" "sa_viewer" {
+  project = "cohesive-amp-310912"
+  role    = "roles/viewer"
+  member  = "serviceAccount:${google_service_account.test_sa.email}"
 }
